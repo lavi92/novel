f0f4874afc780dadb98d413a0bce963ad4572dc4
Lavinia Bacila <lavinia_bacila@yahoo.ro>
2013-06-26 10:28:30 +0300

    Add commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Book::Application.config.secret_token = '6e55ae882e059f3eae5a27560436f528f47fc3717bbe37f1268aa4c8ca4b1a8f9eba0b512545940427de5d705834000c0dac9df4de090d498cb60bd71ef4c81f'
